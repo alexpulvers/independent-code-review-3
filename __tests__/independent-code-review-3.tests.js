eb5f491a6adc3ae741019e04f7dff866f3483759
apul8377 <apulvers@my.chemeketa.edu>
2020-07-16 19:23:31 -0700

    git fixedgit status

---
+++ __tests__/independent-code-review-3.tests.js
@@ -0,0 +1,8 @@
+import { reportAge } from "./../src/main.js";
+
+describe("reportAge()", () => {
+  test("should return a value for each property in reportAge()", () => {
+    let age = 28;
+    reportAge(age);
+  });
+});
